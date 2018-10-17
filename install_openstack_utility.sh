b6c4a0ae0ec1c9fd75683b40472caa9e45b94161
diwakarthyagaraj <diwakardmn@gmail.com>
2018-10-17 14:25:54 -0500

    Chart/Dockerfile for Openstack Utility Container
    Change-Id: I6644824776f7890c2475904ba3404e281e10e54e
    Co-authored-by: Sreejith Punnapuzha <Sreejith.Punnapuzha@outlook.com>

---
+++ install_openstack_utility.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -xe
+
+#NOTE: Lint and package chart
+: ${OSH_INFRA_PATH:="../openstack-helm-infra"}
+
+make openstack-utility
+
+helm  upgrade --install openstack-utility openstack-utility --namespace=utility
+
+#NOTE: Wait for deploy
+../${OSH_INFRA_PATH}/tools/deployment/common/wait-for-pods.sh utility
+
+#NOTE: Validate Deployment info
+kubectl get -n utility jobs --show-all
+kubectl get -n utility secrets
+kubectl get -n utility configmaps
