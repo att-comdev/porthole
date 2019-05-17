0570c69f83d9a3b3244677d14e378b6d50752396
Jagan Kavva <jk330k@att.com>
2019-05-17 13:46:03 -0500

    Chart/Dockerfile for etcdctl Utility Container
    This etcdctl utility container will help the Operation user
    to check the state/stats of etcdctl resources in the K8s Cluster
    
    Change-Id: If0c46b9c19bf8492be3e92c8c215cc9980886981

---
+++ install_etcdctl_utility.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xe
+
+make etcdctl-utility
+
+helm  upgrade --install etcdctl-utility etcdctl-utility --namespace=utility
+
+#NOTE: Validate Deployment info
+kubectl get -n utility jobs --show-all
+kubectl get -n utility secrets
+kubectl get -n utility configmaps
